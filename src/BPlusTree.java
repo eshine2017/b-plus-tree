7df95cfd3d5a2446853f6c7b8c8b75bfbec3370d
eshine2017 <deng.pan.sjtu@gmail.com>
2017-11-03 21:33:51 -0400

    start BPlusTree

---
+++ src/BPlusTree.java
@@ -0,0 +1,30 @@
+import NodeStructure.*;
+
+import java.util.ArrayList;
+
+/**
+ * Main B+ tree structure, supporting insert(key, value), search(key) and search(key1, key2)
+ */
+public class BPlusTree {
+
+    /** construct a new B+ tree with order m */
+    public BPlusTree(int m) {
+
+    }
+
+    /** insert a new key-val pair to the tree */
+    public void insert(double key, String val) {
+
+    }
+
+    /** search for all values corresponding to given key */
+    public ArrayList<String> search(double key) {
+        return new ArrayList<>();
+    }
+
+    /** search for all the pairs whose key are between key1 and key2 */
+    public ArrayList<Pair> search(double key1, double key2) {
+        return null;
+    }
+
+}
